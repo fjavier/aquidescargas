3c91884bd27e9fee5c5c06f39b64fb276f6cea8d
Francisco Briceño <francisco@agilityfeat.com>
2014-02-08 20:33:33 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aquidesacargas::Application.config.secret_key_base = 'a9ea037fe08be2c14f5e900b6fc0f7843c5328984e4357837489d10a6306002aced3c0bf5ecc09eeebc02ec671546375a8d8209f3c0f55aa214553448895f0c6'
