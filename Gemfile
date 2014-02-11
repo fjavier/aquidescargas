2fad35fad6ecb4f4ef3d4bba845995dd2430d063
Francisco Briceño <francisco@agilityfeat.com>
2014-02-11 13:07:53 -0600

    Configuracion de Devise 1

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'turbolinks'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
+gem 'devise'
 
 group :development do
 	# Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Aquidesacargas::Application.routes.draw do
+  devise_for :users
    root 'paginas_estaticas#bienvenido'
   get "paginas_estaticas/bienvenido"
   # The priority is based upon order of creation: first created -> highest priority.
