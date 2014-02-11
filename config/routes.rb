800b42e56bb8004627365d82d3b40685f95f003e
Francisco Briceño <francisco@agilityfeat.com>
2014-02-11 10:07:02 -0600

    toHeroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Aquidesacargas::Application.routes.draw do
+   root 'paginas_estaticas#bienvenido'
+  get "paginas_estaticas/bienvenido"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
